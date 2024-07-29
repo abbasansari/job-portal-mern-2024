628bb1ec1ddd5b88eabdece1d8f325cf070a9153
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-07-29 14:58:15 +0500

    DB Connection

--- backend/app.js
+++ backend/app.js
@@ -1,7 +1,28 @@
 import express from "express";
 import { config } from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import colors from "colors";
+import morgan from "morgan";
+import mongoose from "mongoose";
 
 const app = express();
 config({ path: "./config/config.env" });
+
+// database connection
+mongoose
+  .connect(process.env.MONGOOSE_URI)
+  .then(() => {
+    console.log("MongoDB connected successfully".bgGreen);
+  })
+  .catch((error) => {
+    console.error("Error connecting to MongoDB:".bgRed, error);
+  });
+
+//middlewares
+app.use(cors());
+app.use(express.json());
+app.use(cookieParser());
+app.use(morgan("dev"));
 
 export default app;
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import app from "./app.js";
 
 app.listen(3000, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
+  console.log(`Server running on port ${process.env.PORT}`.bgGreen);
 });
 
 // Middleware for handling CORS requests
