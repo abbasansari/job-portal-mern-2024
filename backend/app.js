cf389ae8ce7318e8b39168e18c6fa67de3eb6702
abbasansari <53369349+abbasansari@users.noreply.github.com>
2024-07-29 15:01:53 +0500

    Error Middleware

--- backend/app.js
+++ backend/app.js
@@ -25,4 +25,16 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(morgan("dev"));
 
+//Error Middleware
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500; // Corrected variable name and syntax
+  const message = err.message || "Internal Server Error"; // Corrected variable name
+
+  return res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
+
 export default app;
